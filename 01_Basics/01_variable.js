96c7c56fdf07c625ace92ac23a231763165b54bb
navraj2503 <164631054+navraj2503@users.noreply.github.com>
2026-07-01 07:26:41 +0000

    variables and data types

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountNo=2523
+let accountName="Rahul"
+let emailId="l@l.com"
+let accPass="nonchalant12"
+let empAdd="sikkim"
+console.table([accountNo,accPass,accountName,emailId,empAdd]);
+console.log(typeof emailId)
+// console.log(typeof accountNo)
+let ValueinString= String(accountNo)
+console.log( ValueinString);
